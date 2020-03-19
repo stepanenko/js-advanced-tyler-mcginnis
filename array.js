04088d27f7c33e5dc140ed11663ce0e23151e964
stepanenko <serhiy.stepanenko@aiesec.net>
2020-03-19 14:28:47 +0200

    new method

--- array.js
+++ array.js
@@ -1,6 +1,6 @@
 
 function array() {
-  let arr = {};
+  let arr = Object.create(array.prototype);
   arr.length = 0;
 
   for (key in arguments) {
@@ -9,6 +9,29 @@ function array() {
   }
 
   return arr;
+}
+
+array.prototype.push = function(element) {
+  this[this.length] = element;
+  this.length++;
+  return this.length;
+}
+
+array.prototype.unshift = function(element) {
+  for (let i = this.length; i >= 0; i--) {
+    this[i] = this[i - 1];
+  }
+  this[0] = element;
+  this.length++;
+  return this.length;
+}
+
+array.prototype.pop = function() {
+
+}
+
+array.prototype.filter = function() {
+
 }
 
 const a = array('John', 'Bill');
